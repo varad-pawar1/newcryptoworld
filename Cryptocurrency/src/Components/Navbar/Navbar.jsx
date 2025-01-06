c57952a182bc340ef1e7319bfc8875fdfec98c1c
varad-pawar1 <varadpawarm@gmail.com>
2025-01-06 17:15:59 +0530

    update the code work on feedback3

--- Cryptocurrency/src/Components/Navbar/Navbar.jsx
+++ Cryptocurrency/src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,9 @@ import Search from "../Search/Search";
 const Navbar = ({ searchValuefn, sortByfn }) => {
   return (
     <div className={styles.nav}>
-      {/* Clicking on the title redirects to the homepage */}
       {/* <h1 onClick={() => (window.location.reload())}>Crypto-World</h1> */}
       <h1>Crypto-World</h1>
 
-      {/* Passing searchValuefn function to Search component for handling search functionality */}
       <Search searchValuefn={searchValuefn} sortByfn={sortByfn} />
     </div>
   );
